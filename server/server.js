dcfcc3aaf83e90e06f2a330ad3633b005a96e9a3
nukoha01 <123347337+nukoha01@users.noreply.github.com>
2023-08-13 20:34:18 -0400

    Update server.js with auth

--- server/server.js
+++ server/server.js
@@ -1,19 +1,27 @@
-
 const express = require('express');
 const apiRouter = require('./routes');
 
 const app = express();
 
+const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjoic3BpbiIsImlhdCI6MTY5MTk3MjM5M30.O7OzHJfkIxdZSrE2Kxo9-yYvHMLaSulWWcvN5r7jBxU'; // Replace with the token you generated
+
+// Middleware to authenticate the token
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token !== authToken) {
+        return res.sendStatus(401); // Unauthorized
+    }
+
+    next(); // Pass the control to the next middleware or route handler
+};
 
 app.use(express.json());
 
-app.use('/api/size', apiRouter);
-//change above to zipsize -> app.use('/api/size', apiRouter);
-
+// Use the authentication middleware
+app.use('/api/', authenticateToken, apiRouter);
 
 app.listen(process.env.PORT || '3000', () => {
-
     console.log(`Server is running on port: ${process.env.PORT || '3000'}`);
-
 });
-
